a1413d7658c0008f5f5b2bfb56c666ac3ec2fee5
HonestGiftless <113893391+HonestGiftless@users.noreply.github.com>
2024-01-19 01:03:46 +0800

    Add files via upload

---
+++ Master-classes/class1/script.js
@@ -0,0 +1,19 @@
+const colorBox = document.getElementById('colorBox');
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    
+    return color;
+}
+
+function changeColor() {
+    const newColor = getRandomColor();
+    colorBox.style.backgroundColor = newColor;
+}
+
+colorBox.addEventListener('click', changeColor);
+// setInterval(changeColor, 3000);
